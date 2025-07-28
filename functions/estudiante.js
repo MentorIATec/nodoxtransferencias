80504e0ee00f71378e8b07208905bb3738f1ed39
MentorIA <kareng@tec.mx>
2025-07-27 18:13:25 -0600

    Update estudiante.js

--- functions/estudiante.js
+++ functions/estudiante.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-export default async function handler(req, res) {
+export default async (req, res) => {
   // Configurar CORS primero
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
